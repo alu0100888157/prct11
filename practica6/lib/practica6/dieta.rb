8cb0089626cf6a6503b13a44c9cec46053d90523
nico_apache <nico10907@hotmail.com>
2016-11-01 21:49:23 +0000

    creando get_pocentaje y get_titulo

--- practica6/lib/practica6/dieta.rb
+++ practica6/lib/practica6/dieta.rb
@@ -8,4 +8,14 @@ class Dieta
     def initialize(descripcion, titulo, porcentaje, porcion, gramos, vct, p_proteina, p_grasas, p_hidratos)
         @descripcion, @titulo, @porcentaje, @porcion, @gramos, @vct, @p_proteina, @p_grasas, @p_hidratos = descripcion, titulo, porcentaje, porcion, gramos, vct, p_proteina, p_grasas, p_hidratos
     end
+    
+    def get_titulo
+        # Devuelve el título del menú
+        "#{@titulo}"
+    end
+    
+    def get_porcentaje
+        # Devuelve el porcentaje de la ingesta diaria
+        "(#{@porcentaje}%)"
+    end
 end
