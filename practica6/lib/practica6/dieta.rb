fb9f861b1c60b73b771f680dda9f163aea4a33c5
nico_apache <nico10907@hotmail.com>
2016-11-01 21:30:28 +0000

    solucionando errores

---
+++ practica6/lib/practica6/dieta.rb
@@ -0,0 +1,10 @@
+
+require "practica6/version"
+
+class Dieta
+    #Se tiene acceso de lectura y escritura a todos los atributos
+    attr_accessor :descripcion, :titulo, :porcentaje, :porcion, :gramos, :vct, :p_proteina, :p_grasas, :p_hidratos
+    def initialize(descripcion, titulo, porcentaje, porcion, gramos, vct, p_proteina, p_grasas, p_hidratos)
+        @descripcion, @titulo, @porcentaje, @porcion, @gramos, @vct, @p_proteina, @p_grasas, @p_hidratos = descripcion, titulo, porcentaje, porcion, gramos, vct, p_proteina, p_grasas, p_hidratos
+    end
+end
