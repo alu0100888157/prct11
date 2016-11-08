5bdd6bba2c416f6fab800c989077a4ef92360f71
Angel Luis Morales Hernandez <alu0100888157@ull.edu.es>
2016-11-08 21:25:54 +0000

    .F
    Failures:
    
      1) List Pruebas de la parte lista ->Debe existir un objeto y estar inicializado.
         Failure/Error: expect(diet).not_to be_nil
    
         NameError:
           undefined local variable or method `diet' for #<RSpec::ExampleGroups::List::PruebasDeLaParteLista:0x0000000379d990>
         # ./spec/practica6_spec.rb:11:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00169 seconds (files took 0.14231 seconds to load)
    2 examples, 1 failure
    
    Failed examples:
    
    rspec ./spec/practica6_spec.rb:10 # List Pruebas de la parte lista ->Debe existir un objeto y estar inicializado.

--- practica6/lib/practica6/lista.rb
+++ practica6/lib/practica6/lista.rb
@@ -1,4 +1,7 @@
 
 class List 
-    
+    attr_accessor :head
+        def initialize
+            @head = @tail = nil
+        end
 end
--- practica6/spec/practica6_spec.rb
+++ practica6/spec/practica6_spec.rb
@@ -2,12 +2,13 @@ require "spec_helper"
 
 
 describe List do
+    #diet = List.new()
     context "Pruebas de la parte lista" do 
     it "has a version number" do
       expect(Practica6::VERSION).not_to be nil
     end
-    # it "->Debe existir un objeto y estar inicializado." do
-    #   expect(diet).not_to be_nil
-    # end
+    it "->Debe existir un objeto y estar inicializado." do
+      expect(diet).not_to be_nil
+    end
   end
 end
