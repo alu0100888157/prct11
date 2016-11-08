79fc78442a85c922170995e60ff0d98aea61ced7
Angel Luis Morales Hernandez <alu0100888157@ull.edu.es>
2016-11-08 22:26:48 +0000

    ......
    Finished in 0.00394 seconds (files took 0.14722 seconds to load)
    6 examples, 0 failures

--- practica6/lib/practica6/lista.rb
+++ practica6/lib/practica6/lista.rb
@@ -14,11 +14,11 @@ class List
         end
         def to_s(letras)
             @letras = letras
+            @aux = 0
             @ob = head
             @var = ""
             while @ob != nil do
                 objeto = Dieta.new(@ob.value[0],@ob.value[1],@ob.value[2],@ob.value[3],@ob.value[4],@ob.value[5],@ob.value[6], @ob.value[7],@ob.value[8])
-
                 @var += "#{@letras[@aux]}) #{objeto.to_s}"
                @aux = @aux + 1
                 @ob = @ob.next
