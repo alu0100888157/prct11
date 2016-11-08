8a2eb6c284fd4d3e1ff39d9454bf3a012f89e2cf
Angel Luis Morales Hernandez <alu0100888157@ull.edu.es>
2016-11-08 21:53:04 +0000

    `load': /home/ubuntu/workspace/LPP3/tdd-menu-lpp-41-1/practica6/spec/practica6_spec.rb:18: unterminated string meets end of file (SyntaxError)

--- practica6/spec/practica6_spec.rb
+++ practica6/spec/practica6_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 
 describe List do
     diet = List.new()
-    vector = ["BANANA"]
+    vector = [["Macarrones", "Spaguetti"], "Media mañana", "40-45", ["1 1/2 cucharón","2 cazo"], ["200", "100"], "356'7", "10", "20", "30"]
     context "Pruebas de la parte lista" do 
     it "has a version number" do
       expect(Practica6::VERSION).not_to be nil
@@ -13,6 +13,9 @@ describe List do
     end
     it "->Debe existir un objeto y estar inicializado." do
       expect(vector).not_to be_nil
+    end
+    it "->Debe mostrar una dieta" do
+      expect(diet.to_s).to eq("Media mañana (40-45%)\n- Macarrones: 1 1/2 cucharón, 200g\n- Spaguetti: 2 cazo, 100g\nV.C.T | %    356'7kcal | 10% - 20% - 30%)
     end
   end
 end
