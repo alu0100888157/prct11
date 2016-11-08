ad29361889533cbe42be1519fa8bbb1b8e47c79b
Angel Luis Morales Hernandez <alu0100888157@ull.edu.es>
2016-11-08 21:12:09 +0000

    /home/ubuntu/workspace/LPP3/tdd-menu-lpp-41-1/practica6/spec/practica6_spec.rb:4:in `<top (required)>': uninitialized constant List (NameError)

--- practica6/lib/practica6.rb
+++ practica6/lib/practica6.rb
@@ -1,4 +1,5 @@
 require "practica6/version"
+require "practica6/dieta"
 
 module Practica6
   # Your code goes here...
--- practica6/spec/practica6_spec.rb
+++ practica6/spec/practica6_spec.rb
@@ -1,55 +1,13 @@
 require "spec_helper"
-require "./lib/practica6/dieta"
 
-describe Dieta do
-  #before :all do
-  @descripcion = ["Macarrones", "Spaguetti"]
-  @titulo = "Media mañana"
-  @porcentaje = "40-45"
-  @porcion = ["1 1/2 cucharón","2 cazo"]
-  @gramos = ["200", "100"]
-  @vct = "356'7"
-  @p_proteina = "10"
-  @p_grasas = "20"
-  @p_hidratos = "30"
-  
-    diet = Dieta.new(@descripcion, @titulo, @porcentaje, @porcion, @gramos, @vct, @p_proteina, @p_grasas, @p_hidratos)
-    context "Pruebas de la parte dieta" do 
+
+describe List do
+    context "Pruebas de la parte lista" do 
     it "has a version number" do
       expect(Practica6::VERSION).not_to be nil
     end
     it "->Debe existir un objeto y estar inicializado." do
       expect(diet).not_to be_nil
-    end
-    it "->Probando descripcion" do
-      expect(diet.get_descripcion()).to eq("Macarrones\nSpaguetti\n")
-    
-    end
-    it "->Probando porcentaje" do
-      expect(diet.get_porcentaje()).to eq("(40-45%)")
-    end
-    it "->Probando titulo" do
-      expect(diet.get_titulo()).to eq("Media mañana")
-    end
-    it "->Probando conjunto de platos" do
-      expect(diet.get_cjto_platos()).to eq("- Macarrones: 1 1/2 cucharón, 200g\n- Spaguetti: 2 cazo, 100g\n")
-    end
-    it "->Probando VCT" do
-      expect(diet.get_vct()).to eq("V.C.T | %    356'7kcal | ")
-    end
-    it "->Probando proteina" do
-      expect(diet.get_p_proteina()).to eq("10%")
-    end
-    
-    it "->Probando grasas" do
-      expect(diet.get_p_grasas()).to eq("20%")
-    end
-    
-    it "->Probando hidratos" do
-      expect(diet.get_p_hidratos()).to eq("30%")
-    end
-    it "-> Probando to_s" do
-      expect(diet.to_s()).to eq("Media mañana (40-45%)\n- Macarrones: 1 1/2 cucharón, 200g\n- Spaguetti: 2 cazo, 100g\nV.C.T | %    356'7kcal | 10% - 20% - 30%")
     end
   end
 end
