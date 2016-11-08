c018ef7468b9f62ba22c29804e581693024a1806
Angel Luis Morales Hernandez <alu0100888157@ull.edu.es>
2016-11-08 22:23:17 +0000

    ....F.
    Failures:
    
      1) List Pruebas de la parte lista ->Debe existir un objeto y estar inicializado.
         Failure/Error: expect(letras).not_to be_nil
    
         NameError:
           undefined local variable or method `letras' for #<RSpec::ExampleGroups::List::PruebasDeLaParteLista:0x000000008a9440>
         # ./spec/practica6_spec.rb:25:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00449 seconds (files took 0.12586 seconds to load)
    6 examples, 1 failure
    
    Failed examples:
    
    rspec ./spec/practica6_spec.rb:24 # List Pruebas de la parte lista ->Debe existir un objeto y estar inicializado.

--- practica6/spec/practica6_spec.rb
+++ practica6/spec/practica6_spec.rb
@@ -21,6 +21,9 @@ describe List do
     it "->Debe existir un objeto y estar inicializado." do
       expect(vector2).not_to be_nil
     end
+    it "->Debe existir un objeto y estar inicializado." do
+      expect(letras).not_to be_nil
+    end
     it "->Debe mostrar una dieta" do
       expect(diet.to_s).to eq("Media mañana (40-45%)\n- Macarrones: 1 1/2 cucharón, 200g\n- Spaguetti: 2 cazo, 100g\nV.C.T | %    356'7kcal | 10% - 20% - 30%Mañana (30%)\n- Langosta: 3 cuchara, 100g\n- Bocadillo: 2 tenedor, 50g\nV.C.T | %    350kcal | 10% - 20% - 30%")
     end
