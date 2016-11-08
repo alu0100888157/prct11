47ab611e658df9de9c02bf28c987df98e39db379
Angel Luis Morales Hernandez <alu0100888157@ull.edu.es>
2016-11-08 22:25:36 +0000

    .....F
    Failures:
    
      1) List Pruebas de la parte lista ->Debe mostrar una dieta
         Failure/Error: @var += "#{@letras[@aux]}) #{objeto.to_s}"
    
         TypeError:
           no implicit conversion from nil to integer
         # ./lib/practica6/lista.rb:22:in `[]'
         # ./lib/practica6/lista.rb:22:in `to_s'
         # ./spec/practica6_spec.rb:29:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00485 seconds (files took 0.14262 seconds to load)
    6 examples, 1 failure
    
    Failed examples:
    
    rspec ./spec/practica6_spec.rb:28 # List Pruebas de la parte lista ->Debe mostrar una dieta

--- practica6/lib/practica6/lista.rb
+++ practica6/lib/practica6/lista.rb
@@ -12,15 +12,15 @@ class List
         
             @tail = node
         end
-        def to_s
-        
+        def to_s(letras)
+            @letras = letras
             @ob = head
             @var = ""
             while @ob != nil do
                 objeto = Dieta.new(@ob.value[0],@ob.value[1],@ob.value[2],@ob.value[3],@ob.value[4],@ob.value[5],@ob.value[6], @ob.value[7],@ob.value[8])
 
-                @var += "#{objeto.to_s}"
-               
+                @var += "#{@letras[@aux]}) #{objeto.to_s}"
+               @aux = @aux + 1
                 @ob = @ob.next
             end
             "#{@var}"
--- practica6/spec/practica6_spec.rb
+++ practica6/spec/practica6_spec.rb
@@ -26,7 +26,7 @@ describe List do
       expect(letras).not_to be_nil
     end
     it "->Debe mostrar una dieta" do
-      expect(diet.to_s).to eq("Media mañana (40-45%)\n- Macarrones: 1 1/2 cucharón, 200g\n- Spaguetti: 2 cazo, 100g\nV.C.T | %    356'7kcal | 10% - 20% - 30%Mañana (30%)\n- Langosta: 3 cuchara, 100g\n- Bocadillo: 2 tenedor, 50g\nV.C.T | %    350kcal | 10% - 20% - 30%")
+      expect(diet.to_s(letras)).to eq("Media mañana (40-45%)\n- Macarrones: 1 1/2 cucharón, 200g\n- Spaguetti: 2 cazo, 100g\nV.C.T | %    356'7kcal | 10% - 20% - 30%Mañana (30%)\n- Langosta: 3 cuchara, 100g\n- Bocadillo: 2 tenedor, 50g\nV.C.T | %    350kcal | 10% - 20% - 30%")
     end
   end
 end
