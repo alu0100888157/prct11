9f89cc7a8d83a73d4dbdd84bf5659b85fa26a64e
Angel Luis Morales Hernandez <alu0100888157@ull.edu.es>
2016-11-15 22:12:01 +0000

    ........................
    Finished in 0.00658 seconds (files took 0.11327 seconds to load)
    24 examples, 0 failures

--- practica6/lib/practica6/lista.rb
+++ practica6/lib/practica6/lista.rb
@@ -1,11 +1,12 @@
 Node = Struct.new(:value, :next, :prev)
-
-
-class List 
-    attr_accessor :head
+class List
+    attr_accessor :head, :tail
         def initialize
             @head = @tail = nil
         end
+        
+        
+        
         def add_to_list(value)
             node = Node.new(value)
         
@@ -16,16 +17,33 @@ class List
             end
             @tail = node
         end
+        
         def to_s(letras)
             @letras = letras
-            @aux = 0
             @ob = head
+            @aux = 0
             @var = ""
             while @ob != nil do
-                objeto = Dieta.new(@ob.value[0],@ob.value[1],@ob.value[2],@ob.value[3],@ob.value[4],@ob.value[5],@ob.value[6], @ob.value[7],@ob.value[8])
-                @var += "#{@letras[@aux]}) #{objeto.to_s}"
-               @aux = @aux + 1
-                @ob = @ob.next
+                if(@ob.value[9]==nil) 
+                    
+                        objeto = Dieta.new(@ob.value[0],@ob.value[1],@ob.value[2],@ob.value[3],@ob.value[4],@ob.value[5],@ob.value[6], @ob.value[7],@ob.value[8])
+                        @var += "#{@letras[@aux]}) #{objeto.to_s}"
+                       @aux = @aux + 1
+                        @ob = @ob.next
+                    
+                else 
+                        if(@ob.value[10]==nil)
+                            ob_food = Alimentos.new(@ob.value[0],@ob.value[1],@ob.value[2],@ob.value[3],@ob.value[4],@ob.value[5],@ob.value[6], @ob.value[7],@ob.value[8], @ob.value[9])
+                            @var +="#{@letras[@aux]}) #{ob_food.to_s}"
+                            @aux = @aux + 1
+                            @ob = @ob.next
+                        else
+                            ob_edad = Edad.new(@ob.value[0],@ob.value[1],@ob.value[2],@ob.value[3],@ob.value[4],@ob.value[5],@ob.value[6], @ob.value[7],@ob.value[8], @ob.value[9], @ob.value[10])
+                            @var += "#{@letras[@aux]}) #{ob_edad.to_s}"
+                            @aux = @aux + 1
+                            @ob = @ob.next
+                        end
+                end
             end
             "#{@var}"
         end
--- practica6/spec/practica6_spec.rb
+++ practica6/spec/practica6_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 
 describe List do
     diet = List.new()
-    letras = ["a","b"]
+    letras = ["a","b", "c", "d"]
     vector = [["Macarrones", "Spaguetti"], "Media mañana", "40-45", ["1 1/2 cucharón","2 cazo"], ["200", "100"], "356'7", "10", "20", "30"]
     diet.add_to_list(vector)
     vector2 = [["Langosta", "Bocadillo"], "Mañana", "30", ["3 cuchara", "2 tenedor"], ["100","50"], "350", "10", "20", "30"]
@@ -36,7 +36,7 @@ describe List do
         expect(letras).not_to be_nil
       end
       it "->Debe mostrar una dieta" do
-        expect(diet.to_s(letras)).to eq("a) Media mañana (40-45%)\n- Macarrones: 1 1/2 cucharón, 200g\n- Spaguetti: 2 cazo, 100g\nV.C.T | %    356'7kcal | 10% - 20% - 30%b) Mañana (30%)\n- Langosta: 3 cuchara, 100g\n- Bocadillo: 2 tenedor, 50g\nV.C.T | %    350kcal | 10% - 20% - 30%")
+      expect(diet.to_s(letras)).to eq("a) Media mañana (40-45%)\n- Macarrones: 1 1/2 cucharón, 200g\n- Spaguetti: 2 cazo, 100g\nV.C.T | %    356'7kcal | 10% - 20% - 30%b) Mañana (30%)\n- Langosta: 3 cuchara, 100g\n- Bocadillo: 2 tenedor, 50g\nV.C.T | %    350kcal | 10% - 20% - 30%c) Dieta -> Verdura\nMedia mañana (40-45%)\n- Macarrones: 1 1/2 cucharón, 200g\n- Spaguetti: 2 cazo, 100g\nV.C.T | %    356'7kcal | 10% - 20% - 30%d) Dieta -> De 8-10 años\nMedia mañana (40-45%)\n- Macarrones: 1 1/2 cucharón, 200g\n- Spaguetti: 2 cazo, 100g\nV.C.T | %    356'7kcal | 10% - 20% - 30%")
       end
     end
 end
